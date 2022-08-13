2052775f8ebe8dc7efd73b355be788865c337f6e
st452004 <97547500+samriddhtenguria@users.noreply.github.com>
2022-08-13 10:33:38 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+import joblib 
+
+#load the joblib model 
+model_nb = joblib.load('youtube_nationalist_comments')
+
+#user input 
+st.title("NATIONALIST COMMENTS CLASSIFIER")
+ip = st.text_input("Enter your message:")
+
+#predict if the entered message is nationalist or not 
+op = model_nb.predict([ip])
+if st.button('IS NATIONALIST'):
+  st.title(op[0])  #prints the output as true if comment is nationalist and false otherwise  
